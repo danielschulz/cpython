25393e721c0cb3e5f057f2c9a08302854ec35089
Daniel Schulz danielschulz2005@hotmail.com <danielschulz2005@hotmail.com>
2020-12-28 15:49:35 +0100

    took dow version to 256

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -22,12 +22,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	7
-#define PY_MICRO_VERSION	20201228
+#define PY_MICRO_VERSION	256
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"2.7.20201228"
+#define PY_VERSION      	"2.7.256"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository). Empty
